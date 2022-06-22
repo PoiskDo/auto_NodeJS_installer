891649f90af01632bd81f4772285b3116c4a76a3
dorincop <101493930+dorincop@users.noreply.github.com>
2022-06-22 19:14:48 +0900

    Update nodejs_installer.sh

--- nodejs_installer.sh
+++ nodejs_installer.sh
@@ -1,5 +1,5 @@
 echo "Node.JS를 설치하는중..."
-echo "(비닐번호가 필요할수도 있음)"
+echo "(비밀번호가 필요할수도 있음)"
 sudo apt-get update
 sudo apt-get install curl
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
